718a62c31c3f9d4a9d84ed2ac6ffeb5fe2c56de9
StevenJimenez18 <73972220+StevenJimenez18@users.noreply.github.com>
2021-04-27 16:34:34 -0400

    Add files via upload

---
+++ Web_Fundamentals/JQuery/jquerypractice.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    
+    $('a').click(function(){
+    alert('You are now leaving the page');
+    })
+
+    $('h4').click(function(){
+        alert('hello');
+    })
+
+    $('a').click(function(){
+        alert('hello');
+    })
+});
+
+
