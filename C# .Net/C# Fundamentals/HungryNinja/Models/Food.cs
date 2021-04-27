1f3057fae1cccd1e6a46911519a42bff2d05fb54
StevenJimenez18 <73972220+StevenJimenez18@users.noreply.github.com>
2021-04-27 17:29:33 -0400

    Add files via upload

---
+++ C# .Net/C# Fundamentals/HungryNinja/Models/Food.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+
+namespace HungryNinja.Models
+{
+    public class Food
+    {
+        public string Name;
+        public int Calories;
+        // Foods can be Spicy and/or Sweet
+        public bool IsSpicy;
+        public bool IsSweet;
+        // add a constructor that takes in all four parameters: Name, Calories, IsSpicy, IsSweet
+
+        public Food(string name, int calories, bool isspicy, bool issweet)
+        {
+            Name = name;
+            Calories = calories;
+            IsSpicy = isspicy;
+            IsSweet = issweet;
+        }
+
+
+    }
+}
---
+++ C# .Net/C# Fundamentals/HungryNinja/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using HungryNinja.Models;
+
+namespace HungryNinja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ninja Kyle = new Ninja(0);
+            Buffet newFood = new Buffet();
+
+            Kyle.Eat(newFood.Serve());
+            Kyle.Eat(newFood.Serve());
+            Kyle.Eat(newFood.Serve());
+            Kyle.Eat(newFood.Serve());
+            Kyle.Eat(newFood.Serve());
+            Kyle.Eat(newFood.Serve());
+            Kyle.Eat(newFood.Serve());
+            Kyle.Eat(newFood.Serve());
+            Kyle.Eat(newFood.Serve());
+            Kyle.Eat(newFood.Serve());
+            Kyle.Eat(newFood.Serve());
+            Kyle.Eat(newFood.Serve());
+
+        }
+    }
+}
